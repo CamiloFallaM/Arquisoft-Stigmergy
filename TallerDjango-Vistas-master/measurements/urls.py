574dd374add7ff2358b67f0dce4688df9a021543
CamiloFallaM <c.falla@uniandes.edu.co>
2021-11-18 10:41:15 -0500

    App

---
+++ TallerDjango-Vistas-master/measurements/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns =  [
+        path('list/', views.get_measurements, name='measurementList'),
+        path('get/', views.get_measurement, name='measurement'),
+        path('delete/<int:pk>/', views.delete_measurement, name='measurement_delete'),
+        path('update/<int:pk>/<int:new_value>/', views.update_measurement, name='measurement_update')
+]
